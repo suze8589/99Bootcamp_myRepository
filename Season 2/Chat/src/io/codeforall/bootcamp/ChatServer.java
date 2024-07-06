7214f71bc0b2edcc3fe543d8c83e5d4cd2961671
Susana Eduardo <suzana_mrt@hotmail.com>
2024-07-06 18:46:51 +0100

    still working

--- Season 2/Chat/src/io/codeforall/bootcamp/ChatServer.java
+++ Season 2/Chat/src/io/codeforall/bootcamp/ChatServer.java
@@ -107,10 +107,7 @@ public class ChatServer {
                 System.out.println("Message send to client: " + message);
                 server.broadcast(message, this);
 
-                BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in));
-                String userName = systemIn.readLine();
-                System.out.println(userName);
-                client.outPut.println("username: " + userName);//send the username to the server//needs to be in the server not the client!!!
+                //client.outPut.println("username: " + userName);//send the username to the server//needs to be in the server not the client!!!
             }
         } catch (IOException e) {
             out.println("Error handling client connection: " + e.getMessage());
